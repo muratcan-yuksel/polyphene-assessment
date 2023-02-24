900dcb710e3f2e6d37bc57a89c9f703370ef37e1
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-02-24 17:23:06 +0300

    done

--- src/components/Assessments/GraphQLAssessment/GraphQLAssessmentTable.js
+++ src/components/Assessments/GraphQLAssessment/GraphQLAssessmentTable.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import { gql, useQuery } from "@apollo/client";
 
 const GET_CAKES = gql`
@@ -13,32 +13,48 @@ const GET_CAKES = gql`
 `;
 
 export default function GraphQLAssessmentTable() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   const { loading, error, data } = useQuery(GET_CAKES);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
   console.log(data.cakes);
+
+  const filteredCakes = data.cakes.filter((cake) =>
+    cake.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <table id="reduxAssessmentTable">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Title</th>
-          <th>Description</th>
-          <th>Image</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.cakes.map((item) => (
-          <tr key={item.id}>
-            <td>{item.id}</td>
-            <td>{item.title}</td>
-            <td>{item.description}</td>
-            <td>
-              <img src={item.image} alt={item.title} className="w-80" />
-            </td>
+    <>
+      <input
+        type="text"
+        placeholder="Search cakes..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="w-80  my-4"
+      />
+      <table id="reduxAssessmentTable">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Image</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {filteredCakes.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.title}</td>
+              <td>{item.description}</td>
+              <td>
+                <img src={item.image} alt={item.title} className="w-80" />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
--- src/components/Assessments/ScssAssessment/index.js
+++ src/components/Assessments/ScssAssessment/index.js
@@ -1,30 +1,35 @@
 import React from "react";
 
-export default function StylingAssessment() {
+export default function ScssAssessment() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center ">
-      <div className="box h-5/6 w-3/6 border flex flex-col">
-        <div className="top h-2/6 bg-lilac flex flex-row border border-red-600">
-          <div className="w-1/4 text-green  bg-lilac h-full flex justify-center items-center hover:justify-end hover:items-end hover:bg-green hover:text-lilac">
-            <p className="">John</p>
+    <div className="grid grid-cols-1 divide-y divide-none text-left">
+      <h3 className="font-medium leading-tight text-3xl m-2 text-blue-600 w-fit">
+        1. Styling
+      </h3>
+      <div className="h-screen w-screen flex justify-center items-center ">
+        <div className="box h-5/6 w-3/6 border flex flex-col">
+          <div className="top h-2/6 bg-lilac flex flex-row border border-red-600">
+            <div className="w-1/4 text-green  bg-lilac h-full flex justify-center items-center hover:justify-end hover:items-end hover:bg-green hover:text-lilac">
+              <p className="">John</p>
+            </div>
+            <div className="w-2/4 text-lilac bg-green h-full flex justify-end items-end hover:justify-center hover:items-center hover:bg-lilac hover:text-green">
+              <p className=" ">doe</p>
+            </div>
+            <div className="w-1/4 bg-lilac text-green h-full flex justify-center items-center hover:justify-end hover:items-end hover:bg-green hover:text-lilac">
+              <p className=" ">John</p>
+            </div>
           </div>
-          <div className="w-2/4 text-lilac bg-green h-full flex justify-end items-end hover:justify-center hover:items-center hover:bg-lilac hover:text-green">
-            <p className=" ">doe</p>
-          </div>
-          <div className="w-1/4 bg-lilac text-green h-full flex justify-center items-center hover:justify-end hover:items-end hover:bg-green hover:text-lilac">
-            <p className=" ">John</p>
-          </div>
-        </div>
-        <div className="bottom h-4/6  flex flex-row w-full ">
-          <div className="left border border-red-600 w-2/3 bg-green flex justify-end items-end text-lilac hover:text-green hover:bg-lilac hover:justify-center hover:items-center">
-            <p>doe</p>
-          </div>
-          <div className="right border border-red-600 w-1/3 flex flex-col">
-            <div className="h-2/4 bg-green text-lilac flex justify-end items-end hover:text-green hover:bg-lilac hover:justify-center hover:items-center ">
+          <div className="bottom h-4/6  flex flex-row w-full ">
+            <div className="left border border-red-600 w-2/3 bg-green flex justify-end items-end text-lilac hover:text-green hover:bg-lilac hover:justify-center hover:items-center">
               <p>doe</p>
             </div>
-            <div className="h-2/4 bg-lilac text-green flex justify-center items-center hover:justify-end hover:items-end hover:text-lilac hover:bg-green ">
-              <p>john</p>
+            <div className="right border border-red-600 w-1/3 flex flex-col">
+              <div className="h-2/4 bg-green text-lilac flex justify-end items-end hover:text-green hover:bg-lilac hover:justify-center hover:items-center ">
+                <p>doe</p>
+              </div>
+              <div className="h-2/4 bg-lilac text-green flex justify-center items-center hover:justify-end hover:items-end hover:text-lilac hover:bg-green ">
+                <p>john</p>
+              </div>
             </div>
           </div>
         </div>
